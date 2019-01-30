da39c699aac57da4e12dc224c6cf71d0d9e0ffed
Jonathan Levi <jonathan1996levi@gmail.com>
2019-01-30 15:46:31 +0000

    add deployment params

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
 const Ecclesia = artifacts.require("./Ecclesia.sol");
 const Registration = artifacts.require("./Registration.sol");
 
-module.exports = function(deployer) {
+// Deployment Arguments
+const RegistrationArgs = [
+	1568851200,
+	1569110400,
+	"secp256k1",
+	"SHA1"
+];
+
+module.exports = (deployer, network, accounts) => {
 	deployer.deploy(Ecclesia);
-	deployer.deploy(Registration);
+	deployer.deploy(Registration, ...RegistrationArgs);
 };
