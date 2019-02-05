18f23ccc7129af60c7841eaf58ddb9b91cc38360
Jonathan Levi <jonathan1996levi@gmail.com>
2019-02-05 22:20:09 +0000

    add getter for modulus

--- contracts/CredentialGeneration.sol
+++ contracts/CredentialGeneration.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.4.25 <0.6.0;
 
+import {RSAAccumulator} from './RSAAccumulator.sol';
+
 contract CredentialGeneration {
   // TODO: move to library
   // stores IPFS hashes
@@ -10,14 +12,15 @@ contract CredentialGeneration {
     uint8 size;
   }
 
-  // TODO: EA provides modulus, initial accumulator
+  RSAAccumulator public rsaAccumulator;
+
   // TODO: using Oraclize, retrieve Ver. func for Voter
   // TODO: check signature
-  // TODO: update accumulator
-
+  
   constructor(
     uint256 openingTime_,
-    uint256 closingTime_
+    uint256 closingTime_,
+    bytes memory accumulatorModulus_
   ) public {
     // TODO: DRY, move this to abstract?
     // TODO: link with other contract? i.e. require previous phase to close
@@ -30,6 +33,19 @@ contract CredentialGeneration {
       "Closing Time must be after Opening Time."
     );
 
+    rsaAccumulator = new RSAAccumulator(accumulatorModulus_);
+  }
 
+  // add secret prime to accumulator
+  /* function addToAccumulator() {
+  ...
+  } */
+
+  // getter for accumulator modulus
+  function getAccumulatorModulus() public view returns (uint256[8]) {
+    // Different from provided bytes argument in constructor
+    // Provides the number used in the accumulator to check the computation
+    // done in the contract.
+    return rsaAccumulator.getN();
   }
 }
