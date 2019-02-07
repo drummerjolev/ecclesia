af119e64a968118341d0ad0fc1c397d5934ed266
Jonathan Levi <jonathan1996levi@gmail.com>
2019-02-07 16:32:23 +0000

    add index.js boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+import { default as Web3 } from 'web3';
+import { default as contract } from 'truffle-contract';
+import { default as EcclesiaLib } from './lib/ecclesia';
+
+// TODO: import contracts from ./build/...
+
+export default function (host, port) {
+  // return new EcclesiaLib(...);
+}
